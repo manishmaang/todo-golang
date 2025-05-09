7acb875db4b4dd120388d7eed12f156838d356a2
Manish Pandey <manishpandeycoding@gmail.com>
2025-05-10 00:42:37 +0530

    learned about packages, connection setup and models in the golang

---
+++ config/database.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options" // The correct import
+)
+
+const connection_string = "mongodb://localhost:27017/"
+const db_name = "golang-todo"
+
+var DB *mongo.Database
+func init() { //special functions jo ki sirf pure application me ek baar run hote hai that to jb application start ho rha hota hai
+
+	//client option
+	clientOption := options.Client().ApplyURI(connection_string).SetMaxPoolSize(10) 
+	// options is the package imported in line 9, options.client() is a function which creates a setting struct(obj) which has connection configuration
+
+	//connection with mongoDB
+	client, err := mongo.Connect(context.TODO(), clientOption)
+	// (context.todo)so it basically defines some setting for each db operation but context.todo is like empty setting where nothing is handled but will be later on
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("Conneceted with the mongoDB");
+
+    DB = client.Database(db_name)
+}
+
+func GetDB() *mongo.Database{
+	return DB
+}
---
+++ models/todo.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	// primitive is the package being imported , and it provides the mongodb data types for our schema to store data
+)
+
+type ToDo struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	Title     string             `bson:"title" json:"title"`
+	Description string           `bson:"description" json:"description"`
+	Completed bool               `bson:"completed" json:"completed"`
+}
